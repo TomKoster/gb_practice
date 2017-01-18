129f753943524283485d5581258c3a1a82d006be
TomKoster <indigoshw@gmail.com>
2017-01-19 00:58:51 +0300

    add hanoi solver

--- 002_hanoi_tower/h.hs
+++ 002_hanoi_tower/h.hs
@@ -8,3 +8,15 @@ stepCountRecursivly towerHeigh
   | towerHeigh <= 0 = 0
   | otherwise       = 2 * stepCountRecursivly(towerHeigh - 1) + 1
 
+
+type TowerHeigh = Integer
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoiMoveSet :: TowerHeigh -> Peg -> Peg -> Peg -> [Move]
+hanoiMoveSet 0 _ _ _ = []
+hanoiMoveSet n from to buffer = topsToBuffer ++ [bottomMove] ++ topsFromBuffer
+  where
+    topsToBuffer = hanoiMoveSet (n - 1) from buffer to
+    bottomMove = (from, to)
+    topsFromBuffer = hanoiMoveSet (n - 1) buffer to from
